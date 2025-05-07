00e5bea35b4aa3ffcd300493813c9ac28bc42c9a
QuickJack <32240639+whuter@users.noreply.github.com>
2025-05-07 22:22:41 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,14 +1,14 @@
-import { zhihuVideo } from "./zhihu-video.ts";
-import { zhihuQuestions } from "./zhihu-questions.ts";
-import { zhihuSearch } from "./zhihu-search.ts";
 import { weiboSearch } from "./weibo-search.ts";
 import { toutiaoSearch } from "./toutiao-search.ts";
+import { zhihuQuestions } from "./zhihu-questions.ts";
+import { zhihuVideo } from "./zhihu-video.ts";
+import { zhihuSearch } from "./zhihu-search.ts";
 
 export async function init() {
   await weiboSearch();
   await toutiaoSearch();
-  await zhihuVideo();
   await zhihuQuestions();
+  await zhihuVideo();
   // await zhihuSearch(); TODO: API update
 }
 

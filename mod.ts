615ce43d1730cc340274a2e6ee7b7e7d2f80707e
QuickJack <32240639+whuter@users.noreply.github.com>
2025-05-13 13:30:24 +0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { weiboSearch } from "./weibo-search.ts";
 import { toutiaoSearch } from "./toutiao-search.ts";
-import { zhihuQuestions } from "./zhihu-questions.ts";
 import { zhihuVideo } from "./zhihu-video.ts";
+import { zhihuQuestions } from "./zhihu-questions.ts";
 import { zhihuSearch } from "./zhihu-search.ts";
 
 export async function init() {
